ea2b1429518dfa5e39e3c57ee96a85f654e9099a
Roman T <tselenkor@gmail.com>
2019-04-09 13:17:49 +0300

    index added some code

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser")
 
 const app = express();
-mongoose.connect("mongodb://localhost/persons-db")
+mongoose.connect("mongodb://localhost/persons-db", { useNewUrlParser: true })
 
 app.use(bodyParser.json())
 app.use("/api", require("./api"))
 
 app.listen(4000, ()=>{
     console.log("server is listening")
-}) 
+})
