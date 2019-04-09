2d33261d506d324f22f53e370fdb6bc8b4f70947
Roman T <tselenkor@gmail.com>
2019-04-09 14:35:11 +0300

    changed some code

--- api.js
+++ api.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const router = express.Router()
+const Person = require("./person")
 
 router.get("/persons", (req, res)=>{
     res.send({method: "GET"})
 })
 
 router.post("/persons", (req, res)=>{
-    res.send({method: "POST"})
-})
+    Person.create(req.body)
+    .then(person=> res.send(person))
+    })
 
 router.put("/persons/:id", (req, res)=>{
     res.send({method: "PUT"})
@@ -18,3 +20,4 @@ router.delete("/persons/:id", (req, res)=>{
 })
 
 module.exports = router
+
